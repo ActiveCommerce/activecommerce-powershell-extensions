28000c85d128d9816ce2b9c10ef350b725e95c74
Adam Brauer <abrauer@hansondodge.com>
2016-03-16 14:36:42 -0500

    [AC-1081 Added Low Stock report. Also Orders report to use Get-AcSites for shop context.

---
+++ src/ActiveCommerce.SitecorePowerShell/Cmdlets/GetAcProduct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Management.Automation;
+using Sitecore.Ecommerce.DomainModel.Products;
+using Microsoft.Practices.Unity;
+
+namespace ActiveCommerce.SitecorePowerShell.Cmdlets
+{
+    [Cmdlet("Get", "AcProduct")]
+    public class GetAcProduct : Cmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        public string Code { get; set; }
+
+        [Parameter]
+        public string TypeName { get; set; }
+
+        public GetAcProduct()
+        {
+            TypeName = typeof(Sitecore.Ecommerce.DomainModel.Products.ProductBaseData).FullName;
+        }
+
+        protected override void ProcessRecord()
+        {
+            Type type = null;
+            if (!LanguagePrimitives.TryConvertTo<Type>(TypeName, out type))
+            {
+                throw new ArgumentException("TypeName", string.Format("{0} is not a valid type", TypeName));
+            }
+            var productRepository = Sitecore.Ecommerce.Context.Entity.Resolve<IProductRepository>();
+            var getMethod = productRepository.GetType().GetMethod("Get", new[] { typeof(string) });
+            var genericMethod = getMethod.MakeGenericMethod(type);
+            var product = genericMethod.Invoke(productRepository, new object[] { Code });
+            WriteObject(product);
+        }
+    }
+}
